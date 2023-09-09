077a4e21965c42dacb63ef9a91c7a1efffc968b4
yasingunay <av.yasingunay@gmail.com>
2023-09-09 17:03:26 +0200

    search substring list added

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django import forms
 from django.http import HttpResponse
 from .util import markdown_to_html
 from .util import get_entry, separate_markdown_content, list_entries
@@ -27,15 +28,24 @@ def entry(request, title):
     
         
 def search(request):
+    sub_entries= []
     if request.method == "POST":
-        title = request.POST["q"]
+        query = request.POST["q"]
         # If the query matches the name of an encyclopedia entry, the user should be redirected to that entry’s page.
-        if title in list_entries():
-            html_content = markdown_to_html(title)
+        if query in list_entries():
+            html_content = markdown_to_html(query)
             header, main_content = separate_markdown_content(html_content)
-            return render(request, "encyclopedia/entry.html", {"header": header, "main_content": main_content, "title" : title})
+            return render(request, "encyclopedia/entry.html", {"header": header, "main_content": main_content, "title" : query})
         else:
-            return render(request, "encyclopedia/error.html")
+            # If the query does not match the name of an encyclopedia entry, 
+            # the user should instead be taken to a search results page 
+            # that displays a list of all encyclopedia entries that have the query as a substring. 
+            for entry in list_entries():
+                if query.lower()in entry.lower():
+                    sub_entries.append(entry)
+            return render(request, "encyclopedia/search.html", {"entries": sub_entries, "query" : query})
+            
+           
     else:
         # Handle GET request
         return render(request, "encyclopedia/index.html")
